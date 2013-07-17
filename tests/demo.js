b0a3de616a9604d68b92f8acdae269357c74fea4
simonfan <simonxd@gmail.com>
2013-07-17 14:01:29 -0300

    removed old examples from demo

--- tests/demo.js
+++ tests/demo.js
@@ -1,48 +1,5 @@
 define(['anima', 'jquery'], function(Anima, $) {
 
-/*	window.a = Anima.build({
-		$el: $('#first'),
-		initial: 'fadeIn',
-
-		Animas: {
-			fadeIn: {
-				opacity: 1,
-				zIndex: 1,
-			},
-			fadeOut: {
-				opacity: 0,
-				zIndex: 0
-			}
-		},
-
-		animaOptions: {
-			fadeIn: {
-				duration: 1000,
-				__before: {
-					display: 'block',
-					zIndex: 1,
-				}
-			},
-			fadeOut: {
-				duration: 3000,
-				__after: function($el) {
-					$el.css('display', 'none');
-				}
-			}
-		}
-	});
-
-
-	a.on('fadeOut', function() {
-		console.log('stopped at fadeOut');
-	});
-
-	a.on('enter:fadeIn', function() {
-		console.log('entering fadeIn')
-	});
-*/
-
-
 
 	$('#first').anima({
 		initial: 'fadeIn',
@@ -101,5 +58,5 @@ define(['anima', 'jquery'], function(Anima, $) {
 
 
 
-	$('#first').anima('flow',['fadeIn','halt','fadeOut'])
+	$('#first').anima('flow',['fadeIn','halt','fadeOut','fadeHalf'])
 });
